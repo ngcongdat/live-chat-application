3a21a24f9e30f1d824dd70c6bf383ef41ba70621
TinyNguyen <ncd.ccpn@gmail.com>
2019-05-01 09:29:17 +0700

    Project ChatLib

---
+++ ChatLib/src/com/entity/Client.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.entity;
+
+import java.io.Serializable;
+import java.net.Socket;
+
+/**
+ *
+ * @author tiny
+ */
+public class Client implements Serializable {
+    private String username, password;
+    private Socket socket;
+
+    @Override
+    public String toString() {
+        return username; //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public void setSocket(Socket socket) {
+        this.socket = socket;
+    }
+    
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getType() {
+        return password;
+    }
+
+    public void setType(String password) {
+        this.password = password;
+    }
+
+    public Client() {
+    }
+
+    public Client(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public Client(String username, String password, Socket socket) {
+        this.username = username;
+        this.password = password;
+        this.socket = socket;
+    }
+}
---
+++ ChatLib/src/com/entity/Server.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.entity;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author tiny
+ */
+public class Server implements Serializable {
+    
+    private String host;
+    private int port;
+
+    public Server() {
+    }
+
+    public Server(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+    
+}
